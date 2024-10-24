77bbc66332e41a9e255bc8ea6458dbc6af920dcd
Ubuntu <dfuse@grass-router-new.2yu0qgli2x4ezi5f0i2o2ei55f.lx.internal.cloudapp.net>
2024-10-24 09:47:50 +0000

    add

--- grass_exporter.py
+++ grass_exporter.py
@@ -2,6 +2,7 @@ import os
 import time
 import logging
 import psutil
+import requests
 from prometheus_client import start_http_server, Gauge
 from dotenv import load_dotenv
 
@@ -15,6 +16,18 @@ logging.basicConfig(filename='hl_exporter.log', level=logging.INFO, format='%(as
 hl_cpu_usage = Gauge('hl_cpu_usage', 'CPU usage percentage')
 hl_memory_usage = Gauge('hl_memory_usage', 'Memory usage percentage')
 hl_disk_usage = Gauge('hl_disk_usage', 'Disk usage percentage')
+hl_health_status = Gauge('hl_health_status', 'Health status (1 for OK, 0 for not OK)')
+
+def check_health():
+    try:
+        response = requests.get('https://labs.asxn.xyz/health')
+        if response.status_code == 200 and response.json().get('status') == 'ok':
+            return 1
+        else:
+            return 0
+    except Exception as e:
+        logging.error(f"Error checking health status: {e}")
+        return 0
 
 def monitor_system_resources():
     while True:
@@ -23,16 +36,17 @@ def monitor_system_resources():
         hl_memory_usage.set(memory.percent)
         disk = psutil.disk_usage('/')
         hl_disk_usage.set(disk.percent)
-        logging.info(f"Updated metrics - CPU: {psutil.cpu_percent()}%, Memory: {memory.percent}%, Disk: {disk.percent}%")
+        health_status = check_health()
+        hl_health_status.set(health_status)
+        logging.info(f"Updated metrics - CPU: {psutil.cpu_percent()}%, Memory: {memory.percent}%, Disk: {disk.percent}%, Health: {health_status}")
         time.sleep(60)
 
 if __name__ == "__main__":
-    print("ssss")
     # Start Prometheus HTTP server on port 8086
     logging.info("Starting Prometheus HTTP server on port 8086")
     start_http_server(8086)
     logging.info("Prometheus HTTP server started on port 8086")
-    print("ssss")
+
     # Start monitoring thread
     try:
         monitor_system_resources()
